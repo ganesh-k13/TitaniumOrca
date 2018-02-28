8bb849f2c1a6e6aaf741d98f5cb3ef098386bb3d
Ganesh K <ganesh3597@gmail.com>
2018-02-28 18:58:14 +0530

    New Tables

--- analyser.py
+++ analyser.py
@@ -4,14 +4,15 @@ import socket
 import traceback
 from ipaddress import ip_address
 import json
-
+from tabulate import tabulate
+from pprint import pprint
 class TitaniumOrca:
 	
 	def __init__(self, filename):
 		self.filename = filename
 		self.malicious_ip = dict()
 		self.attackers = list()
-	
+		self.port_list = dict()
 	def __check_if_port_scan(self, port_list):
 		return len(port_list) - len(set([i for i in range(min(port_list), max(port_list)+1)])) <= 10
 	
@@ -19,6 +20,7 @@ class TitaniumOrca:
 		# Open pcap file
 		with open(self.filename, 'rb') as f:
 			pcap = dpkt.pcap.Reader(f) # Parse file
+			print("Parsing....")
 			for ts, buf in pcap:
 				try:
 					# Extract TCP data if present else fail silently
@@ -32,43 +34,62 @@ class TitaniumOrca:
 					
 					# Find port number and src, dest IPs
 					# [TODO]: FIND PORT, dest
+					port = tcp.dport
 					src_ip_addr_str = socket.inet_ntoa(ip.src)
 					
 					# IF Packet is a TCP-SYN
-					if(syn_flag == True and ack_flag == False):	
+					if(syn_flag == True and ack_flag == False):
 						# malicious_ip maps: ip->[SYN_count, non_SYN_count]
-						self.malicious_ip[ip.src] = self.malicious_ip.get(ip.src, [0, 0])
+						self.malicious_ip[ip.src] = self.malicious_ip.get(ip.src, [0, 0, []])
 						self.malicious_ip[ip.src][0]+=1
+						self.malicious_ip[ip.src][2].append(port)
 					else:
-						self.malicious_ip[ip.src] = self.malicious_ip.get(ip.src, [0, 0])
+						self.malicious_ip[ip.src] = self.malicious_ip.get(ip.src, [0, 0, []])
 						self.malicious_ip[ip.src][1]+=1
-					print(syn_flag, ack_flag, ip.dst, src_ip_addr_str)
+						self.malicious_ip[ip.src][2].append(port)
+					# print(syn_flag, ack_flag, ip.dst, src_ip_addr_str, port)
+					
 				except:
 					traceback.print_exc() # [IMP]: Uncomment while developing
 		
-			return self.__detect_attackers()
+			return self.__table(self.__detect_attackers(), ["IP Adress", "Port"])
+	
+	def get_count(self):
+		ip_count = list()
+		for ip, count in self.malicious_ip.items():
+			if(count[0] > 3 * count[1]):
+				ip_count.append([socket.inet_ntoa(ip), count[0]])
+		return self.__table(ip_count, ["IP Adress", "Count"])
 	
 	def __detect_attackers(self):
 		
+#		pprint(self.malicious_ip.items())
 		for ip, count in self.malicious_ip.items():
 			if(count[0] > 3 * count[1]):
-				self.attackers.append([ip, count[0]])
+				for port in count[2]:
+					self.port_list[socket.inet_ntoa(ip)] = self.port_list.get(socket.inet_ntoa(ip), [])
+					self.port_list[socket.inet_ntoa(ip)].append(port)
+					self.attackers.append([socket.inet_ntoa(ip), int(port)])
 		
 		return self.attackers
 		
 	def to_json(self):
 		with open('malicious_ip.json', 'w') as f:
-			json.dumps(f, self.attackers, indent = 4)
+			json.dumps(f, self.malicious_ip, indent = 4)
 	
 	def __call__(self):
-		return self.__check_if_port_scan(range(10))
+		#print(self.port_list)
+		return (self.__check_if_port_scan(self.port_list['192.168.100.103']))
+	
+	def __table(self, info, headers):
+		return tabulate(info, headers = headers, tablefmt="fancy_grid")
 	
 if __name__ == "__main__":
 	s = TitaniumOrca(sys.argv[1])
-	
-	# for i in s.parse():
-		# print(socket.inet_ntoa(i[0]), i[1])
-		
-	print(s())
+
+	print(s.parse())
+	print(s.get_count())
+	# print(tabulate(s.parse()))
+	# print(s())
 		# print(i)
 	# s.to_json()
