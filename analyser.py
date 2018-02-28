f56fc8a408313b3dbcc4f896ed4a0af88a6ed064
Ganesh K <ganesh3597@gmail.com>
2018-02-28 19:25:59 +0530

    Added features(checks)

--- analyser.py
+++ analyser.py
@@ -6,6 +6,7 @@ from ipaddress import ip_address
 import json
 from tabulate import tabulate
 from pprint import pprint
+
 class TitaniumOrca:
 	
 	def __init__(self, filename):
@@ -13,8 +14,11 @@ class TitaniumOrca:
 		self.malicious_ip = dict()
 		self.attackers = list()
 		self.port_list = dict()
-	def __check_if_port_scan(self, port_list):
-		return len(port_list) - len(set([i for i in range(min(port_list), max(port_list)+1)])) <= 10
+		
+	def check_if_port_scan(self, ip):
+		ports = self.port_list[ip]
+		# return len(ports) - len(set([i for i in range(min(ports), max(ports)+1)])) == 0 # More complex
+		return len(ports) > 1000
 	
 	def parse(self):
 		# Open pcap file
@@ -61,6 +65,9 @@ class TitaniumOrca:
 				ip_count.append([socket.inet_ntoa(ip), count[0]])
 		return self.__table(ip_count, ["IP Adress", "Count"])
 	
+	def get_ips(self):
+		return self.__table(self.attackers, ["IP Adress", "Port"])
+	
 	def __detect_attackers(self):
 		
 #		pprint(self.malicious_ip.items())
@@ -78,8 +85,12 @@ class TitaniumOrca:
 			json.dumps(f, self.malicious_ip, indent = 4)
 	
 	def __call__(self):
+		# Only for checks
 		#print(self.port_list)
-		return (self.__check_if_port_scan(self.port_list['192.168.100.103']))
+		return (self.check_if_port_scan(self.port_list['192.168.100.103']))
+	
+	def get_port_lists(self):
+		return self.port_list
 	
 	def __table(self, info, headers):
 		return tabulate(info, headers = headers, tablefmt="fancy_grid")
@@ -87,9 +98,18 @@ class TitaniumOrca:
 if __name__ == "__main__":
 	s = TitaniumOrca(sys.argv[1])
 
-	print(s.parse())
+	s.parse()
+	
+	print("Malicious IP Addresses with their SYN Packet Counts:")
 	print(s.get_count())
-	# print(tabulate(s.parse()))
+	
+	if(input("Do you wish to see the ports attacked/scanned? (y/n) : ") == 'y'):
+		print(s.get_ips())
+	
+	if(input("Do you wish to see if it was a port scan? (y/n): ") == 'y'):
+		for p in s.get_port_lists().keys():
+			print("IP: ", p, " :", s.check_if_port_scan(p))
+ 	# print(tabulate(s.parse()))
 	# print(s())
 		# print(i)
 	# s.to_json()
